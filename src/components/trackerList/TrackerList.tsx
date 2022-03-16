a85ce2c217e684cd0fe6dd0bff5abbc6e57e8e84
Clément Roig <clm.roig@gmail.com>
2022-03-16 19:41:11 +0100

    feat(persistInStorage): add redux-persist and retrieve from localStorage

--- src/components/trackerList/TrackerList.tsx
+++ src/components/trackerList/TrackerList.tsx
@@ -1,22 +1,13 @@
 import { Box, CircularProgress, Typography } from '@mui/material';
-import { useEffect } from 'react';
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useAppSelector } from '../../app/hooks';
 import SliceStatus from '../../models/SliceStatus';
-import { fetchAllTrackers } from '../../store/trackers/trackersSlice';
 import selectTrackers from '../../store/trackers/trackers.selectors';
 
 import TrackerCard from '../TrackerCard/TrackerCard';
 
 function TrackerList() {
   const { status, trackers } = useAppSelector(selectTrackers);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (trackers === undefined) {
-      dispatch(fetchAllTrackers());
-    }
-  }, [trackers]);
 
   return (
     <Box>
--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
 import { HashRouter } from 'react-router-dom';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { PersistGate } from 'redux-persist/integration/react';
+import FullScreenLoading from './app/FullScreenLoading';
 
 ReactDOM.render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <PersistGate loading={<FullScreenLoading />} persistor={persistor}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
   document.getElementById('root')
