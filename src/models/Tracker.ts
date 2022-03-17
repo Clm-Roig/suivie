1ad2710ab9371ff3edf63e8403fdda2d6b6d9679
Clément Roig <clm.roig@gmail.com>
2022-03-17 11:11:27 +0100

    feat(models): add Completion model and update Tracker with it

--- src/models/Tracker.ts
+++ src/models/Tracker.ts
@@ -1,15 +1,32 @@
+import Completion from './Completion';
 import TrackerEntry from './TrackerEntry';
 import TrackerStatus from './TrackerStatus';
+
+/**
+ * A Tracker describes an objective which needs to be accomplished on a daily basis.
+ * To keep tracker of the objective, the trackers references TrackerEntries.
+ *
+ * @interface Tracker
+ * @member {string} id a v4 uuid
+ * @member {string} beginDate when the completions start (can be in the past or the future)
+ * @member {string?} dateHidden when not undefined, specify which day the tracker is ignored. The next day, it's automatically set to "undefined".
+ * @member {number?} duration number of days the tracker is active since beginDate
+ * @member {string} endDate when the Tracker is over
+ * @member {TrackerEntry[]} entries TrackerEntries related to this Tracker
+ * @member {string} name
+ * @member {number?} remainingDays computed attribute describing the number of days before the tracker is over
+ * @member {Completion[]} requiredCompletions objectives to complete the tracker
+ * @member {TrackerStatus} status @see TrackerStatus
+ */
 export default interface Tracker {
-  id: string; // uuid v4
+  id: string;
   beginDate: string;
   dateHidden?: string;
-  defaultQuantity?: number;
   duration?: number;
   endDate?: string;
   entries: TrackerEntry[];
   name: string;
   remainingDays?: number;
+  requiredCompletions: Completion[];
   status: TrackerStatus;
-  unit?: string;
 }
--- src/models/TrackerEntry.ts
+++ src/models/TrackerEntry.ts
@@ -1,5 +1,17 @@
+import Completion from './Completion';
+
+/**
+ * A TrackerEntry is multiple Completions achieved on a precise date
+ *
+ * @interface TrackerEntry
+ * @member {string} id a v4 uuid
+ * @member {Completion[]} completions actions done, can't be empty
+ * @member {string} date when the entry was completed
+ * @member {string} trackerId Tracker the entry is attached to
+ */
 export default interface TrackerEntry {
-  id: string; // uuid v4
+  id: string;
+  completions: Completion[];
   date: string;
-  quantity: number;
+  trackerId: string;
 }
