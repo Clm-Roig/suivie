c99fabda49f65685c0b5496ee8295ac44d59ceec
Clément Roig <clm.roig@gmail.com>
2022-03-21 20:01:55 +0100

    feat(errorDataLoading): add a check on trackers data loading

---
+++ src/app/CustomPersistGate.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import { PersistGate } from 'redux-persist/integration/react';
+import FullScreenLoading from './FullScreenLoading';
+import { persistor } from '../store/store';
+import { useAppSelector } from './hooks';
+import selectTrackers from '../store/trackers/trackers.selectors';
+import isATracker from '../utils/isATracker';
+import { useNavigate } from 'react-router-dom';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const CustomPersistGate: FC<Props> = ({ children }) => {
+  const { trackers } = useAppSelector(selectTrackers);
+  const navigate = useNavigate();
+
+  const handleBeforeLift = () => {
+    for (const tracker of trackers) {
+      const data = isATracker(tracker);
+      if (data.errors.length !== 0) {
+        navigate('./data-error', {
+          state: data
+        });
+      }
+    }
+  };
+
+  return (
+    <PersistGate
+      loading={<FullScreenLoading />}
+      persistor={persistor}
+      onBeforeLift={handleBeforeLift}>
+      {children}
+    </PersistGate>
+  );
+};
+
+export default CustomPersistGate;
