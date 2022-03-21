5a4dba15b39e13444e5383ae69eae788a88295b7
Clément Roig <clm.roig@gmail.com>
2022-03-21 20:01:55 +0100

    feat(errorDataLoading): add a check on trackers data loading

--- src/app/Router.tsx
+++ src/app/Router.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Trackers from '../pages/Trackers';
 import Statistics from '../pages/Statistics';
+import DataError from '../pages/errors/DataError';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Trackers />} />
+      <Route path="/data-error" element={<DataError />} />
       <Route path="/trackers" element={<Trackers />} />
       <Route path="/stats" element={<Statistics />} />
     </Routes>
