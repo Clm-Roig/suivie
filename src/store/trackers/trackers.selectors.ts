3210c8e10e462d8d6834402d0e36a70192c58ea6
Clément Roig <clm.roig@gmail.com>
2022-03-23 20:43:57 +0100

    test(trackerSelector): test and refactor

--- src/store/trackers/trackers.selectors.ts
+++ src/store/trackers/trackers.selectors.ts
@@ -1,65 +1,11 @@
 import { RootState } from '../store';
-import Tracker from '../../models/Tracker';
-import { addDays, differenceInDays, isSameDay } from 'date-fns';
 import TrackerStatus from '../../models/TrackerStatus';
-
-const computeRemainingDays = (beginDate: string, duration: number) => {
-  const estimatedEndDateObj = addDays(new Date(beginDate), duration);
-  const difference = differenceInDays(estimatedEndDateObj, new Date());
-  return difference;
-};
-
-const computeNewStatus = (tracker: Tracker) => {
-  const { remainingDays, entries, status } = tracker;
-  let newStatus = status;
-  // End tracker if needed
-  if (remainingDays !== undefined && remainingDays < 0) {
-    newStatus = TrackerStatus.over;
-  }
-  // Mark Tracker as done if there is a completion for today
-  if (entries.some((e) => isSameDay(new Date(e.date), new Date()))) {
-    newStatus = TrackerStatus.done;
-  }
-
-  return newStatus;
-};
-
-const formatTrackers = (trackers: Tracker[]) => {
-  const newTrackers = trackers.map((t) => {
-    let trackerObj = t as Tracker;
-    const { beginDate, dateHidden, duration } = trackerObj;
-    // Delete dateHidden if it is not today
-    if (dateHidden) {
-      if (!isSameDay(new Date(dateHidden), new Date())) {
-        trackerObj.dateHidden = undefined;
-      }
-    }
-    // Remaining Days
-    if (beginDate && duration) {
-      trackerObj = {
-        ...trackerObj,
-        remainingDays: computeRemainingDays(beginDate, duration)
-      };
-    }
-
-    trackerObj = {
-      ...trackerObj,
-      status: computeNewStatus(trackerObj)
-    };
-
-    return trackerObj;
-  });
-  return newTrackers;
-};
-
-const removeOverTrackers = (trackers: Tracker[]) =>
-  trackers.filter((t) => t.status !== TrackerStatus.over);
-
-const removeDoneTrackers = (trackers: Tracker[]) =>
-  trackers.filter((t) => t.status !== TrackerStatus.done);
-
-const removeHiddenTrackers = (trackers: Tracker[]) =>
-  trackers.filter((t) => t.dateHidden === undefined);
+import {
+  formatTrackers,
+  removeDoneTrackers,
+  removeHiddenTrackers,
+  removeOverTrackers
+} from './utils';
 
 const selectHiddenTrackers = (state: RootState) => {
   const newTrackers = removeOverTrackers(
---
+++ src/store/trackers/utils.spec.ts
@@ -0,0 +1,113 @@
+import { subDays } from 'date-fns';
+import TrackerStatus from '../../models/TrackerStatus';
+import {
+  computeRemainingDays,
+  computeNewStatus,
+  removeOverTrackers,
+  removeDoneTrackers,
+  removeHiddenTrackers,
+  formatTrackers
+} from './utils';
+import { testTracker1, testEntry1 } from './FAKE_DATA';
+
+describe('computeRemainingDays()', () => {
+  it('should return the appropriate remaining days', () => {
+    expect(computeRemainingDays(subDays(new Date(), 10).toString(), 3)).toBe(-7); // started in the past, finished in the past
+    expect(computeRemainingDays(subDays(new Date(), 10).toString(), 50)).toBe(39); // started in the past, finished in the future
+    expect(computeRemainingDays(subDays(new Date(), 20).toString(), 20)).toBe(0); // started in the past, finished today
+    expect(computeRemainingDays(new Date().toString(), 10)).toBe(9); // started today, finished in the future
+  });
+});
+
+describe('computeNewStatus()', () => {
+  it('should be over (beginDate + duration in the past)', () => {
+    const finishedTracker = {
+      ...testTracker1,
+      beginDate: subDays(new Date(), 10).toString(),
+      duration: 9
+    };
+    expect(computeNewStatus(finishedTracker)).toBe(TrackerStatus.over);
+  });
+  it('should be over (remainingDays negative)', () => {
+    const finishedTracker = {
+      ...testTracker1,
+      remainingDays: -5
+    };
+    expect(computeNewStatus(finishedTracker)).toBe(TrackerStatus.over);
+  });
+  it('should be done (one a entry for today)', () => {
+    const doneTracker = {
+      ...testTracker1,
+      entries: [testEntry1]
+    };
+    expect(computeNewStatus(doneTracker)).toBe(TrackerStatus.done);
+  });
+});
+
+describe('remove functions', () => {
+  const trackers = [
+    {
+      ...testTracker1,
+      status: TrackerStatus.over
+    },
+    {
+      ...testTracker1,
+      status: TrackerStatus.done
+    },
+    {
+      ...testTracker1,
+      dateHidden: new Date().toString(),
+      status: TrackerStatus.active
+    }
+  ];
+  describe('removeOverTrackers()', () => {
+    it('should remove the trackers over', () => {
+      expect(removeOverTrackers(trackers).length).toBe(2);
+      const [t1, t2, t3] = trackers; // eslint-disable-line @typescript-eslint/no-unused-vars
+      expect(removeOverTrackers(trackers)).toEqual(expect.arrayContaining([t2, t3]));
+    });
+  });
+  describe('removeDoneTrackers()', () => {
+    it('should remove the trackers over', () => {
+      expect(removeDoneTrackers(trackers).length).toBe(2);
+      const [t1, t2, t3] = trackers; // eslint-disable-line @typescript-eslint/no-unused-vars
+      expect(removeDoneTrackers(trackers)).toEqual(expect.arrayContaining([t1, t3]));
+    });
+  });
+  describe('removeHiddenTrackers()', () => {
+    it('should remove the trackers over', () => {
+      expect(removeHiddenTrackers(trackers).length).toBe(2);
+      const [t1, t2, t3] = trackers; // eslint-disable-line @typescript-eslint/no-unused-vars
+      expect(removeHiddenTrackers(trackers)).toEqual(expect.arrayContaining([t1, t2]));
+    });
+  });
+});
+
+describe('formatTrackers()', () => {
+  it('should format the trackers with appropriate values', () => {
+    const trackerWithDate = {
+      ...testTracker1,
+      dateHidden: subDays(new Date(), 3).toString()
+    };
+    const trackerWithDateToday = {
+      ...testTracker1,
+      dateHidden: new Date().toString()
+    };
+    const trackerWithUndefinedAttributes = {
+      ...testTracker1,
+      dateHidden: undefined,
+      duration: undefined,
+      endDate: undefined,
+      entries: [],
+      remainingDays: undefined,
+      requiredCompletions: []
+    };
+    const res = formatTrackers([
+      trackerWithDate,
+      trackerWithDateToday,
+      trackerWithUndefinedAttributes
+    ]);
+    expect(res[0].dateHidden).toBeUndefined(); // dateHidden removed because it's in the past
+    expect(res[1].dateHidden).toBe(trackerWithDateToday.dateHidden); // dateHidden not removed because it's today
+  });
+});
