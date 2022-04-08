cd8840eb0420884b8d1e213f0f9d3e9d25d13dcf
Clement <clm.roig@gmail.com>
2022-04-08 16:59:05 +0200

    fix(stats): all panels now use same date

--- src/components/Statistics/YearPanel/YearPanel.tsx
+++ src/components/Statistics/YearPanel/YearPanel.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { Box, TextField } from '@mui/material';
 import { DatePicker } from '@mui/lab';
 import Tracker from '../../../models/Tracker';
@@ -7,21 +7,20 @@ import TotalText from './TotalText';
 import YearChart from '../../charts/YearChart/YearChart';
 import { useAppSelector } from '../../../app/hooks';
 import { selectYearEntries } from '../../../store/trackers/trackers.selectors';
-import { endOfYear } from 'date-fns';
+import { endOfYear, startOfYear } from 'date-fns';
 import { TRACKERS_BEGIN_IN } from '../../../config/Constants';
 
 interface Props {
+  beginDate: Date;
+  setBeginDate: (date: Date) => void;
   tracker: Tracker;
 }
 
-const YearPanel: FC<Props> = ({ tracker }) => {
-  const [selectedYear, setSelectedYear] = useState<Date>(new Date());
-  const monthEntries = useAppSelector((state) =>
-    selectYearEntries(state, selectedYear, tracker.id)
-  );
+const YearPanel: FC<Props> = ({ beginDate, setBeginDate, tracker }) => {
+  const monthEntries = useAppSelector((state) => selectYearEntries(state, beginDate, tracker.id));
 
   const handleOnYearChange = (date: unknown) => {
-    if (date) setSelectedYear(date as Date);
+    if (date) setBeginDate(startOfYear(date as Date));
   };
 
   return (
@@ -33,14 +32,14 @@ const YearPanel: FC<Props> = ({ tracker }) => {
           maxDate={endOfYear(new Date())}
           minDate={TRACKERS_BEGIN_IN}
           onChange={handleOnYearChange}
-          value={selectedYear}
+          value={beginDate}
           renderInput={(params) => <TextField sx={{ width: '100%' }} {...params} />}
         />
       </Box>
       <Box sx={{ mb: 1 }}>
         <TotalText completions={getAggregatedCompletions(monthEntries)} />
       </Box>
-      {monthEntries.length > 0 && <YearChart beginDate={selectedYear} entries={monthEntries} />}
+      {monthEntries.length > 0 && <YearChart beginDate={beginDate} entries={monthEntries} />}
     </>
   );
 };
