0214d8867e606593d39712b0a0bcfa23d54d85c7
Clément Roig <clm.roig@gmail.com>
2022-05-04 17:33:14 +0200

    test(markAs): refactor tests

--- src/store/trackers/reducers/markAsActive.ts
+++ src/store/trackers/reducers/markAsActive.ts
@@ -4,11 +4,17 @@ import Tracker from '../../../models/Tracker';
 import TrackerStatus from '../../../models/TrackerStatus';
 import TrackersState from '../TrackersState';
 
+/* eslint-disable no-param-reassign */
+const markTrackerAsActive = (tracker: Tracker) => {
+  tracker.endDate = undefined;
+  tracker.status = TrackerStatus.active;
+};
+/* eslint-enable no-param-reassign */
+
 const markTrackerAsActiveReducer = (state: TrackersState, action: PayloadAction<Tracker['id']>) => {
   const idx = state.trackers.findIndex((t) => t.id === action.payload);
   if (idx !== -1) {
-    state.trackers[idx].endDate = undefined;
-    state.trackers[idx].status = TrackerStatus.active;
+    markTrackerAsActive(state.trackers[idx]);
   }
 };
 const markTrackersAsActiveReducer = (
@@ -17,8 +23,7 @@ const markTrackersAsActiveReducer = (
 ) => {
   const filteredTrackers = state.trackers.filter((t) => action.payload.includes(t.id));
   for (const tracker of filteredTrackers) {
-    tracker.endDate = undefined;
-    tracker.status = TrackerStatus.active;
+    markTrackerAsActive(tracker);
   }
 };
 
--- src/store/trackers/reducers/markAsVisible.spec.ts
+++ src/store/trackers/reducers/markAsVisible.spec.ts
@@ -1,6 +1,9 @@
 import SliceStatus from '../../../models/SliceStatus';
+import Tracker from '../../../models/Tracker';
 import { testTracker1, testTracker1Id, testTracker2, testTracker2Id } from '../FAKE_DATA';
 import trackersReducer, { markTrackerAsVisible } from '../trackersSlice';
+
+const isVisible = (tracker: Tracker) => tracker.dateHidden === undefined;
 
 describe('trackers reducer', () => {
   describe('Mark a tracker as visible', () => {
@@ -18,8 +21,8 @@ describe('trackers reducer', () => {
       );
       const t1 = finalState.trackers.find((t) => t.id === testTracker1Id)!;
       const t2 = finalState.trackers.find((t) => t.id === testTracker2Id)!;
-      expect(t1.dateHidden).toBeUndefined();
-      expect(t2.dateHidden).toBeDefined();
+      expect(isVisible(t1)).toBeTruthy();
+      expect(!isVisible(t2)).toBeTruthy();
     });
   });
 });
