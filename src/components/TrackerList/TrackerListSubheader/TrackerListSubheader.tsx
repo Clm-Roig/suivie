5c2ecdeb085b4cebb2c350a1da3c12bd7d0bb9d2
Clément Roig <clm.roig@gmail.com>
2022-05-04 10:57:03 +0200

    feat(allTrackers): improve ui

--- src/components/TrackerList/TrackerListSubheader/TrackerListSubheader.tsx
+++ src/components/TrackerList/TrackerListSubheader/TrackerListSubheader.tsx
@@ -1,18 +1,30 @@
 import { ListSubheader, Stack } from '@mui/material';
 import { FC } from 'react';
 
+import { useAppSelector } from '../../../app/hooks';
+import ThemeMode from '../../../models/ThemeMode';
+import { selectThemeMode } from '../../../store/theme/theme.selectors';
+
 interface TrackerListSubheaderProps {
   text: string;
   icon: React.ReactNode; // MUI icon
 }
 
-const TrackerListSubheader: FC<TrackerListSubheaderProps> = ({ text, icon }) => (
-  <ListSubheader disableGutters alignItems="center" gap={1} direction="row" component={Stack}>
-    <>
-      {icon}
-      {text}
-    </>
-  </ListSubheader>
-);
+const TrackerListSubheader: FC<TrackerListSubheaderProps> = ({ text, icon }) => {
+  const themeMode = useAppSelector(selectThemeMode);
+  const sx = {
+    bgcolor: themeMode === ThemeMode.DARK ? 'primary.main' : 'primary.main',
+    color: themeMode === ThemeMode.DARK ? 'primary.contrastText' : 'primary.contrastText'
+  };
+
+  return (
+    <ListSubheader sx={sx} alignItems="center" gap={1} direction="row" component={Stack}>
+      <>
+        {icon}
+        {text}
+      </>
+    </ListSubheader>
+  );
+};
 
 export default TrackerListSubheader;
