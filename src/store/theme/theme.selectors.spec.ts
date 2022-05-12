80708641d393367f7fd3c5f25891bd1576683951
Clement <clm.roig@gmail.com>
2022-05-12 13:49:48 +0200

    fix(lint): fix linting error

--- src/store/theme/theme.selectors.spec.ts
+++ src/store/theme/theme.selectors.spec.ts
@@ -1,10 +1,4 @@
-import { addDays, addYears, startOfDay, startOfToday, subDays, subYears } from 'date-fns';
-
-import { SEVEN_DAYS_AGO_DATE, SEVEN_DAYS_AGO_STRING } from '../../config/Constants';
-import SliceStatus from '../../models/SliceStatus';
 import ThemeMode from '../../models/ThemeMode';
-import Tracker from '../../models/Tracker';
-import TrackerStatus from '../../models/TrackerStatus';
 import { createTestStore } from '../createTestStore';
 import { RootState } from '../store';
 import { selectThemeMode } from './theme.selectors';
