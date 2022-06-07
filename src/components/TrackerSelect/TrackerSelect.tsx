a57c07dddf6713f441396784b392890fcc841406
Clément Roig <clm.roig@gmail.com>
2022-06-07 14:03:54 +0200

    fix(trackerSelect): improve label

--- src/components/TrackerSelect/TrackerSelect.tsx
+++ src/components/TrackerSelect/TrackerSelect.tsx
@@ -12,15 +12,16 @@ interface Props {
 }
 
 const TrackerSelect: FC<Props> = ({ onSelect, selectedTrackerId, trackers }) => {
+  const label = 'Sélectionnez un tracker';
   return (
     <Box>
       <FormControl fullWidth>
-        <InputLabel id="tracker-select">Tracker</InputLabel>
+        <InputLabel id="tracker-select">{label}</InputLabel>
         <Select
           labelId="tracker-select-label"
           id="tracker-select"
           value={selectedTrackerId}
-          label="Tracker"
+          label={label}
           onChange={onSelect}>
           {trackers.map((t) => (
             <MenuItem key={t.id} value={t.id}>
