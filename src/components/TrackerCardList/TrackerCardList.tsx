6fd7066f8f14939aded409bd74439468df158737
Clément Roig <clm.roig@gmail.com>
2022-06-23 23:37:00 +0200

    feat(#159): make tracker cards sortable

--- src/components/TrackerCardList/TrackerCardList.tsx
+++ src/components/TrackerCardList/TrackerCardList.tsx
@@ -1,10 +1,12 @@
 import { useAutoAnimate } from '@formkit/auto-animate/react';
 import { CardProps, useTheme } from '@mui/material';
 import { FC } from 'react';
+import { DragDropContext, DragUpdate, Draggable, Droppable } from 'react-beautiful-dnd';
 
-import { useAppSelector } from '../../hooks/redux';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import Tracker from '../../models/Tracker';
 import { selectThemeMode } from '../../store/theme/theme.selectors';
+import { orderTracker } from '../../store/trackers/trackersSlice';
 import TrackerCard from '../TrackerCard/TrackerCard';
 import defaultCardProps from '../TrackerCard/defaultCardProps';
 
@@ -16,17 +18,53 @@ interface Props {
 const TrackerCardList: FC<Props> = ({ trackers, cardProps }) => {
   const themeMode = useAppSelector(selectThemeMode);
   const theme = useTheme();
+  const dispatch = useAppDispatch();
   const [animateRef] = useAutoAnimate<HTMLDivElement>();
   const allCardProps = {
     ...defaultCardProps(themeMode, theme),
     ...cardProps
   };
+
+  const handleOnDragUpdate = (updateResult: DragUpdate) => {
+    const { draggableId: draggedTrackerId, destination } = updateResult;
+    if (destination) {
+      const { index: destinationIndex } = destination;
+      const destinationTrackerId = trackers[destinationIndex].id;
+      dispatch(orderTracker({ sourceTrackerId: draggedTrackerId, destinationTrackerId }));
+    }
+  };
+
   return (
-    <div ref={animateRef}>
-      {trackers.map((t) => (
-        <TrackerCard tracker={t} key={t.id} {...allCardProps} />
-      ))}
-    </div>
+    <DragDropContext
+      onDragUpdate={handleOnDragUpdate}
+      onDragEnd={() => {
+        // onDragUpdate already updated the state
+        return;
+      }}>
+      <Droppable droppableId="trackerCardList">
+        {(provided) => (
+          <div ref={animateRef}>
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {trackers.map((t, idx) => (
+                <Draggable key={t.id} draggableId={t.id} index={idx}>
+                  {(provided) => (
+                    <div ref={provided.innerRef} {...provided.draggableProps}>
+                      <TrackerCard
+                        dragHandleProps={provided.dragHandleProps}
+                        tracker={t}
+                        key={t.id}
+                        {...allCardProps}
+                      />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
