283d1048b8418a4f0fa2f37ad64feb1c4ea3851f
Clément Roig <clm.roig@gmail.com>
2023-03-17 23:54:22 +0100

    remove forms

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -5,10 +5,6 @@ export const JSON_DATA_EXTENSION = '.sv.json'; // SuiVie json
 export const PAYPAL_DONATE_URL =
   'https://www.paypal.com/donate/?business=UF2GH7ZPMLES8&no_recurring=0&item_name=Chaque+don+me+permet+de+continuer+%C3%A0+d%C3%A9velopper+et+%C3%A0+am%C3%A9liorer+l%27application+SuiVie%2C+merci+%21+%3A%29&currency_code=EUR';
 
-export const BUG_REPORT_FORM_URL = 'https://framaforms.org/suivie-rapport-de-bug-1656787432';
-export const IMPROVEMENT_SUGGESTION_FORM_URL =
-  'https://framaforms.org/suivie-suggestion-damelioration-1657039112';
-
 export const DEFAULT_COMPLETION_NAME = 'fois';
 export const APP_NAME = 'SuiVie';
 
